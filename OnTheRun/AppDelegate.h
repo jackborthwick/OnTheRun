3797dfc12bb05e4aed8d7e34cf7cfc34c54d10a1
Jack Borthwick <jackmckeanborthwick@gmail.com>
2015-06-23 13:27:11 -0400

    Initial Commit

---
+++ OnTheRun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnTheRun
+//
+//  Created by Jack Borthwick on 6/23/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnTheRun/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  OnTheRun
+//
+//  Created by Jack Borthwick on 6/23/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
