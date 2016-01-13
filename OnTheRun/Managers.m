3dc80c96599ada02012cb466830252bbaa5fe236
Jack Borthwick <jackmckeanborthwick@gmail.com>
2016-01-13 14:21:57 -0500

    App designed to see food places on a given route, also can be triggered by voice through the word mortality

---
+++ OnTheRun/Managers.m
@@ -0,0 +1,13 @@
+//
+//  Managers.m
+//  OnTheRun
+//
+//  Created by Jack Borthwick on 6/23/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+#import "Managers.h"
+
+@implementation Managers
+
+@end
